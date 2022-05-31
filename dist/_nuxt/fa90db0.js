3a935e35432386642643fdf363582c1f0771448f
jmacalawa <jmacalawa@futurex.ph>
2022-05-31 14:36:40 +0800

    done

--- dist/_nuxt/fa90db0.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{767:function(n,e,d){"use strict";d.r(e);var l=d(93),component=Object(l.a)({},undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
